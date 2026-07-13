14632dbb0bf2e14786a4e4cd7155cde83b8b40ae
mydydat9823 <myduydat9823@gmail.com>
2026-07-13 15:33:41 +0700

    test(article): add test project foundation

---
+++ BTLWEB.Tests/Models/PostStatusTests.cs
@@ -0,0 +1,20 @@
+using BTLWEB.Models;
+
+namespace BTLWEB.Tests.Models;
+
+public class PostStatusTests
+{
+    [Fact]
+    public void All_ShouldContainStatusesRequiredByArticleManagement()
+    {
+        var expectedStatuses = new[]
+        {
+            PostStatus.Draft,
+            PostStatus.Published,
+            PostStatus.Hidden,
+            PostStatus.Archived
+        };
+
+        Assert.Equal(expectedStatuses, PostStatus.All);
+    }
+}
